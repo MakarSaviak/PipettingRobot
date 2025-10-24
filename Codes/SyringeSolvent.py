3fa1a5a2195576d8948358e49d880ef5b01f31a5
Makar Saviak <makar.saviak@tum.de>
2025-10-24 23:37:50 +0200

    commit

--- Codes/SyringeSolvent.py
+++ Codes/SyringeSolvent.py
@@ -2,7 +2,8 @@ from Syringe import Syringe
 from Solvent import Solvent
 
 from pydantic import BaseModel, Field, computed_field, PositiveFloat, model_validator
-from typing import List, Optional
+from typing import Optional, Callable
+
 
 class SyringeSolvent(BaseModel):
     syringe_type: Syringe
@@ -22,3 +23,14 @@ class SyringeSolvent(BaseModel):
             # Use syringe-provided theoretical factor as a starting point
             self.real_correlation_factor = self.syringe_type.theoretical_correlation_factor
         return self
+
+    def linear_mm_to_volume(self, plunger_shift_mm) -> float:
+        """Apply a linear function `fn` to convert plunger shift [mm] to displaced volume [µL]."""
+        effective_mm = max(0.0, plunger_shift_mm - self.backlash_correction)
+        return effective_mm / self.real_correlation_factor
+
+    def general_mm_to_volume(self, plunger_shift_mm: float, fn: Callable[[float], float]) -> float:
+        """Apply a custom function `fn` to convert plunger shift [mm] to displaced volume [µL]."""
+        effective_mm = max(0.0, plunger_shift_mm - self.backlash_correction)
+        return fn(effective_mm)
+

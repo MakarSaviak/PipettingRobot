fb3ef26677b13bc21d755b4a4c33c9ed93da338b
Makar Saviak <makar.saviak@tum.de>
2025-10-24 22:13:28 +0200

    commit

--- Codes/Syringe.py
+++ Codes/Syringe.py
@@ -1,11 +1,20 @@
 import numpy as np
-from pydantic import BaseModel, Field, computed_field
+from pydantic import BaseModel, Field, computed_field, PositiveFloat
 from typing import List, Optional
 
+
 class Syringe(BaseModel):
-    nominal_volume: float
+    nominal_volume: PositiveFloat
     name: str
-    inner_diameter: float # in [mm]
+    inner_diameter: PositiveFloat # in [mm]
 
     def theoretical_correlation_factor(self) -> float:
         return 1 / (np.pi * (self.inner_diameter / 2) ** 2)
+
+
+if __name__ == "__main__":
+    syringe_solvent = Syringe(
+        nominal_volume=100,
+        name = "Hamilton1001",
+        inner_diameter=9.2
+    )

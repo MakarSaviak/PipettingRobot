7475939f88572c394314874a4230b52149aa3c2d
Makar Saviak <makar.saviak@tum.de>
2025-10-24 22:09:36 +0200

    commit

--- Codes/Syringe.py
+++ Codes/Syringe.py
@@ -1,7 +1,11 @@
+import numpy as np
 from pydantic import BaseModel, Field, computed_field
 from typing import List, Optional
 
 class Syringe(BaseModel):
     nominal_volume: float
     name: str
-    inner_diameter: float
+    inner_diameter: float # in [mm]
+
+    def theoretical_correlation_factor(self) -> float:
+        return 1 / (np.pi * (self.inner_diameter / 2) ** 2)
